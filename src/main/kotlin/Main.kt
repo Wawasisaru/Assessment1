b55939d8189646313a24fbe42a0bcef788f4d0b7
Wawasisaru <nsaruwawasi@gmil,.com>
2023-03-09 08:55:26 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,56 @@
+fun main(){
+//   nothing("possible", "Saru") ("Nancy")
+    println()
+    val word = ("Nancy")
+    character()
+
+}
+
+
+
+}
+
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+
+//fun nothing(name1: String, name2: String){
+//    return
+fun character(sec1: String, sec2: String,): String {
+    val call("possible", "saru")
+    return ()
+
+
+}
+
+
+// Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+fun numbers(num1: Int, num2: Int, numb3: Int,): Array<Int>{
+
+
+
+//. Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output.
+
+fun word(name: String,){
+    println(name[0])
+    println(name[1])
+    println(name[2])
+    println(name[3])
+    println(name[4])
+
+}
+//Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+fun fruit(name1: String,name2: String, name3: String):Array<String>{
+    return arrayOf("banana","mango", "orange")
+}
+//Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+fun numbers()
+
+}
